351d68cf7aa4a003ebede7089b9dce913a8c07d3
theborgh <borghiborghi@gmail.com>
2019-07-10 23:06:53 +0200

    Fix form, add App constructor

--- src/App.js
+++ src/App.js
@@ -7,20 +7,22 @@ import Register from './components/Register/Register';
 import './App.css';
 
 class App extends Component {
-
-  state = {
-    solved: false,
-    moveCounter: 0,
-    boardState:
-      [1, 2, 3, 4,
-        5, 6, 7, 8,
-        9, 10, 11, 0,
-        13, 14, 15, 12],
-    squareClicked: new Array(16).fill(false),
-    resetTimer: false,
-    showNewGameModal: false,
-    route: 'signin'
-  };
+  constructor() {
+    super();
+    this.state = {
+      solved: false,
+      moveCounter: 0,
+      boardState:
+        [1, 2, 3, 4,
+          5, 6, 7, 8,
+          9, 10, 11, 0,
+          13, 14, 15, 12],
+      squareClicked: new Array(16).fill(false),
+      resetTimer: false,
+      showNewGameModal: false,
+      route: 'signin'
+    };
+  }
 
   newGame = (numOfShuffles) => {
 
